8763f0c2fbb2493cd55a90c704ddf43c508b8322
Cem Yılmaz <tidusdragonh3art@gmail.com>
2022-09-19 16:44:24 +0300

    version bump

--- packages/dp-icons-misc/dist/Cancel.es.js
+++ packages/dp-icons-misc/dist/Cancel.es.js
@@ -1,12 +1,12 @@
-const e = {
+const t = {
   name: "cancel",
   key: "Cancel",
   type: "misc",
   width: 24,
   height: 24,
-  attributes: void 0,
-  svgPathData: ["m15.7 12 8.3 8.3-3.7 3.7-8.3-8.3L3.7 24 0 20.3 8.3 12 0 3.7 3.7 0 12 8.3 20.3 0 24 3.7z"]
+  attributes: { fill: "currentColor" },
+  svgPathData: ["m15.7 12 8.3 8.31L20.31 24 12 15.7 3.7 24 0 20.31 8.31 12 0 3.7 3.7 0 12 8.31 20.31 0 24 3.7z"]
 };
 export {
-  e as default
+  t as default
 };
