a4d993fdba929acfb6e2e9da9535c84cca0ddf88
Cem Yılmaz <tidusdragonh3art@gmail.com>
2022-12-05 17:12:50 +0300

    version bump

--- packages/dp-icons-react/src/index.ts
+++
@@ -1,5 +0,0 @@
-import Library from './library';
-import {DpIcon as DpIconReact} from "./dp-icon-react";
-import {DpIcon as DpIconVue} from "./dp-icon-vue";
-
-export {Library, DpIconReact, DpIconVue}
