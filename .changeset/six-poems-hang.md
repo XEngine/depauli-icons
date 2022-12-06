c6bfe151050236885153670600ec9eeea023b48e
Cem Yılmaz <c.yilmaz@depaulisystems.com>
2022-12-07 00:22:10 +0300

    q

---
+++ .changeset/six-poems-hang.md
@@ -0,0 +1,6 @@
+---
+"@depaulisystems/glyphs": patch
+"@depaulisystems/glyphs-react": patch
+---
+
+Heck yeah
---
+++ testapp/src/App.server.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import {NavigationMenuVerticalBold} from '@depaulisystems/glyphs'
+import Glyph from "@depaulisystems/glyphs-react";
+
+function AppServer() {
+
+  return (
+    <div className="App">
+          <Glyph icon={NavigationMenuVerticalBold} />
+    </div>
+  )
+}
+
+export default AppServer
