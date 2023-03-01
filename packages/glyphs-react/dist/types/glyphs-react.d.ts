b1ef0345e7f87b7d8f0c73a344d962ad63345b27
Cem Yılmaz <c.yilmaz@depaulisystems.com>
2023-03-02 01:49:07 +0300

    q

--- packages/glyphs-react/dist/types/glyphs-react.d.ts
+++
@@ -1,4 +0,0 @@
-import { FC, SVGProps } from "react";
-import { IGlyphProps } from "./iconType";
-declare const Glyph: FC<IGlyphProps & SVGProps<SVGSVGElement>>;
-export default Glyph;
--- packages/glyphs/dist/types/regular/NavigationMenu4.d.ts
+++
@@ -1,2 +0,0 @@
-import { IconType } from "../iconType";
-export declare const NavigationMenu4: IconType;
