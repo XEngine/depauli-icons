ed8e5f6a2703dac2e9ba8ba3fcde19820b3eb9eb
Cem Yılmaz <c.yilmaz@depaulisystems.com>
2023-07-15 01:08:14 +0300

    test 2

---
+++ packages/glyphs/src/duotone/Logout.ts
@@ -0,0 +1,11 @@
+import {IconType} from "../iconType";
+
+export const Logout: IconType = {
+    name: 'logout',
+    key: 'Logout',
+    type: 'duotone',
+    width: 32,
+    height: 32,
+    viewBox: '0 0 24 24',
+    svgPathData: [{"name":"g","parent":null,"attributes":{"fill":"currentColor"},"children":[{"name":"path","parent":null,"attributes":{"d":"M15 2h-1c-2.83 0-4.24 0-5.12.88S8 5.17 8 8v8c0 2.83 0 4.24.88 5.12S11.17 22 14 22h1c2.83 0 4.24 0 5.12-.88S21 18.83 21 16V8c0-2.83 0-4.24-.88-5.12S17.83 2 15 2z","opacity":".6"},"children":[]},{"name":"path","parent":null,"attributes":{"d":"M8 8c0-1.54 0-2.66.14-3.5H8c-2.36 0-3.54 0-4.27.73S3 7.14 3 9.5v5c0 2.36 0 3.54.73 4.27s1.91.73 4.27.73h.14C8 18.66 8 17.54 8 16V8z","opacity":".4"},"children":[]},{"name":"path","parent":null,"attributes":{"fill-rule":"evenodd","d":"M4.47 11.47a.75.75 0 0 0 0 1.06l2 2a.75.75 0 0 0 1.06-1.06l-.72-.72H14a.75.75 0 0 0 0-1.5H6.81l.72-.72a.75.75 0 1 0-1.06-1.06l-2 2z","clip-rule":"evenodd"},"children":[]}]}],
+}
