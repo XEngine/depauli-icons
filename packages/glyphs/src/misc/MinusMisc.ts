1306ca36a5814b1e3d3209e7da1a69f7e79f14af
Cem Yılmaz <c.yilmaz@depaulisystems.com>
2023-08-28 14:28:01 +0300

    qwe

---
+++ packages/glyphs/src/misc/MinusMisc.ts
@@ -0,0 +1,11 @@
+import {IconType} from "../iconType";
+
+export const MinusMisc: IconType = {
+    name: 'minus',
+    key: 'Minus',
+    type: 'misc',
+    width: 32,
+    height: 32,
+    viewBox: '0 0 24 24',
+    svgPathData: [{"name":"path","attributes":{"fill":"currentColor","d":"M19 13H5v-2h14z"},"children":[]}],
+}
---
+++ packages/glyphs/src/misc/PlusMisc.ts
@@ -0,0 +1,11 @@
+import {IconType} from "../iconType";
+
+export const PlusMisc: IconType = {
+    name: 'plus',
+    key: 'Plus',
+    type: 'misc',
+    width: 32,
+    height: 32,
+    viewBox: '0 0 24 24',
+    svgPathData: [{"name":"path","attributes":{"fill":"currentColor","d":"M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6z"},"children":[]}],
+}
